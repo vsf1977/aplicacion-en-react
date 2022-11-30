500db11e52b46df057460cea0f7491950b9d959b
Vladimir Silva Fuentes <vsilva@asesoftware.com>
2022-11-30 10:30:42 -0500

    Barra de navegacion

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li><a href="#home">Clientes</a></li>
+        <li><a href="#news">Facturas</a></li>
+        <li><a href="#contact">Productos</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
