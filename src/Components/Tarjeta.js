93c4897366a7f6fb867f4fad1ccd8e85c6ff8825
vladimir silva <vsf1977@gmail.com>
2023-02-07 15:13:57 -0500

    cambios para mostrar campos de info de factura

--- src/Components/Tarjeta.js
+++ src/Components/Tarjeta.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react'
 
 export default function Tarjeta() {
 
-  const [clientes, initTarjeta] = useState([]);
+  const [data, initTarjeta] = useState([]);
   const [mensaje, setMensaje] = useState();
   const dataService = new DataService()
   const fetchData = async (route) => {
@@ -16,14 +16,19 @@ export default function Tarjeta() {
     }
   }
 
+  const erase = (p) => {
+    console.log(p.target.id)
+  }
+
+
   useEffect(() => {    
     apiRouteService.getRoute().subscribe(message => {
       if (message) {
         setMensaje(message)
         fetchData(message)
-          .then((res) => {
-            initTarjeta(res)
+          .then((res) => {            
             console.log(res)
+            initTarjeta(res)
           })
           .catch((e) => {
             console.log(e.message)
@@ -36,7 +41,7 @@ export default function Tarjeta() {
 
   return (
     <div className="row container">
-      {clientes.map((item, idx) => {
+      {data.map((item, idx) => {
         return (
           <div className="col-lg-2 col-md-3 col-sm-6 mb-3 tarjetas" key={idx}>
             <div className="card h-100">
@@ -50,17 +55,26 @@ export default function Tarjeta() {
                 </li>
                 <li className="list-group-item">
                   <strong>Fecha Nacimiento:</strong> {item.FechaNacimiento}
+                </li>
+                <li className="list-group-item">
+                  <strong>No de Facturas:</strong> {item.Factura != undefined ? item.Factura.length :0}
                 </li> </div> : <div></div>
                 }
                 { mensaje==='bill' ? <div>                
                 <li className="list-group-item">
-                  <strong>Capital:</strong> {item.IDFactura}
+                  <strong>Nombre Cliente:</strong> {item.Nombre + ' ' + item.Apellidos}
                 </li>
                 <li className="list-group-item">
-                  <strong>Fecha Nacimiento:</strong> {item.Fecha}
+                  <strong>Fecha:</strong> {item.Fecha}
+                </li>
+                <li className="list-group-item">
+                  <strong>Valor Total:</strong> {item.Total}
+                </li>
+                <li className="list-group-item">
+                  <strong>Cantidad:</strong> {item.Cantidad}
                 </li> </div> : <div></div>
                 }
-                <button>Editar</button>
+                <button id= {item.IDFactura != undefined ? item.IDFactura : item.IDCliente} onClick={erase}>Editar</button>
                 <button>Eliminar</button>
               </ul>
             </div>
